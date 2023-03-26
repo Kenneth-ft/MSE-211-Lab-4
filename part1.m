571a48f7e52af57c9307408426472db302f0680e
Sarah Li <sarah_li_7@sfu.ca>
2023-03-26 16:13:19 -0700

    part 1 code

---
+++ part1.m
@@ -0,0 +1,31 @@
+w = size(t1a);
+
+%theta 1
+t1a_x = t1a(1,1:w(2));
+t1a_y = t1a(2,1:w(2));
+t1v_x = t1v(1,1:w(2));
+t1v_y = t1v(2,1:w(2));
+
+%theta 2
+t2a_x = t2a(1,1:w(2));
+t2a_y = t2a(2,1:w(2));
+t2v_x = t2v(1,1:w(2));
+t2v_y = t2v(2,1:w(2));
+
+%theta 3
+t3a_x = t3a(1,1:w(2));
+t3a_y = t3a(2,1:w(2));
+t3v_x = t3v(1,1:w(2));
+t3v_y = t3v(2,1:w(2));
+
+%theta 4
+t4a_x = t4a(1,1:w(2));
+t4a_y = t4a(2,1:w(2));
+t4v_x = t4v(1,1:w(2));
+t4v_y = t4v(2,1:w(2));
+
+%theta 5
+t5a_x = t5a(1,1:w(2));
+t5a_y = t5a(2,1:w(2));
+t5v_x = t5v(1,1:w(2));
+t5v_y = t5v(2,1:w(2));
